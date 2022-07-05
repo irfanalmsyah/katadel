77f5c948820fcb054601a85ccee04110f953fd72
irfanalmsyah <irfanalamsyah2@gmail.com>
2022-07-05 16:16:03 +0700

    make the script more clear

--- main.py
+++ main.py
@@ -6,29 +6,36 @@ import colorama
 from colorama import Back
 colorama.init(autoreset=True)
 
+
+# Validates if the word has KBBI description or not
+# so the user will not get a random unknown word
 def validation(x):
     try:
-        r = requests.get(url = f'https://new-kbbi-api.herokuapp.com/cari/{x}')
-        deskripsi = r.json()['data'][0]['arti'][0]['deskripsi']
+        r = requests.get(url=f'https://new-kbbi-api.herokuapp.com/cari/{x}')
+        description = r.json()['data'][0]['arti'][0]['deskripsi']
     except:
         return False
-    return deskripsi
+    return description
 
-def check(userInput, word):
-    checkList = []
+
+# Compare the inputted word with correct word
+# The list that it return will decide the color for user inputted word later
+def check(input_word, word):
+    check_list = []
     i = 0
     while i < 5:
-        if userInput[i] == word[i]:
-            checkList.append('G')
-        elif userInput[i] in word:
-            checkList.append('B')
+        if input_word[i] == word[i]:
+            check_list.append('G')
+        elif input_word[i] in word:
+            check_list.append('B')
         else:
-            checkList.append('R')
+            check_list.append('R')
         i += 1
-    return checkList
+    return check_list
+
 
 WORDS = words.WORDS
-wordResult = ''
+word_result = ''
 
 while True:
     x = input('\nSelamat datang di Katadel 5 huruf! Apakah kamu ingin bermain? (Y/N)\n')
@@ -39,35 +46,35 @@ while True:
         while True:
             word = random.choice(WORDS)
             valid = validation(word)
-            if valid != False:
+            if valid:
                 break
 
         turn = 1
         while turn < 6:
             while True:
-                userInput = (input(f'\nTebakan ke-{turn}: ')).lower()
-                if userInput == 'clue':
+                input_word = (input(f'\nTebakan ke-{turn}: ')).lower()
+                if input_word == 'clue':
                     print(valid)
-                elif userInput == 'help':
+                elif input_word == 'help':
                     print('Hijau berarti huruf tersebut benar dan posisinya benar\nBiru berarti huruf tersebut benar tetapi posisinya salah\nMerah berarti huruf tersebut salah')
-                elif userInput.isalpha() == False or len(userInput) != 5:
+                elif input_word.isalpha() is False or len(input_word) != 5:
                     print('Input yang diperbolehkan hanya 5 huruf')
-                elif userInput not in WORDS:
+                elif input_word not in WORDS:
                     print('Kata tersebut tidak ada dalam KBBI')
                 else:
                     break
-            result = check(userInput, word)
+            result = check(input_word, word)
             i = 0
             while i < 5:
                 if result[i] == 'G':
-                    wordResult += Back.GREEN + userInput[i]
+                    word_result += Back.GREEN + input_word[i]
                 elif result[i] == 'B':
-                    wordResult += Back.BLUE + userInput[i]
+                    word_result += Back.BLUE + input_word[i]
                 elif result[i] == 'R':
-                    wordResult += Back.RED + userInput[i]
+                    word_result += Back.RED + input_word[i]
                 i += 1
-            print(wordResult)
-            wordResult = ''
+            print(word_result)
+            word_result = ''
             if result == ['G', 'G', 'G', 'G', 'G']:
                 print(f'Kamu menang!\nJawabannya adalah: {word.upper()}')
                 break
@@ -77,10 +84,3 @@ while True:
             turn += 1
     else:
         print("\nInput Kamu tidak dapat dipahami")
-
-    
-
-
-
-
-
